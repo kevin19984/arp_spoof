2c8eccc3beaa0389a3eb9d937057dfd117346e76
kevin19984 <kevin19984@naver.com>
2019-10-23 03:09:11 -0700

    Add arpheader.h

---
+++ arpheader.h
@@ -0,0 +1,16 @@
+#pragma once
+#pragma pack(1)
+
+struct arp_header {
+  struct	arphdr ea_hdr;	/* fixed-size header */
+  u_char	arp_sha[6];	/* sender hardware address */
+  uint32_t	arp_spa;	/* sender protocol address */
+  u_char	arp_tha[6];	/* target hardware address */
+  uint32_t	arp_tpa;	/* target protocol address */
+};
+
+struct etherarp {
+  struct ether_header eth;
+  struct arp_header arp;
+};
+
